7ddd041ba63fde8f9052cfeb0bc894cb7b621b17
Julien Melissas <julien.melissas@gmail.com>
2018-10-05 11:40:52 -0400

    Changing per-product meta key to `require_order_proxy`, adding checkout field functions.

--- woocommerce-order-proxies.php
+++ woocommerce-order-proxies.php
@@ -17,6 +17,16 @@ class WC_Order_Proxies {
 		add_action( 'woocommerce_product_options_general_product_data', [ &$this, 'write_panel' ] );
 		// Process the Admin Panel Saving
 		add_action( 'woocommerce_process_product_meta', [ &$this, 'write_panel_save' ] );
+
+		// Add the custom order field
+		add_action( 'woocommerce_after_order_notes', [ &$this, 'add_checkout_field' ] );
+
+		// Validate & Save the order field
+		add_action( 'woocommerce_checkout_process', [ &$this, 'validate_checkout_field' ] );
+		add_action ( 'woocommerce_checkout_update_order_meta', [ &$this, 'save_checkout_field' ] );
+
+		// Display the proxy in the order backend
+		add_action ( 'woocommerce_admin_order_data_after_billing_address', [ &$this, 'display_order_proxy_order_admin' ] );
 	}
 
 	/**
@@ -28,7 +38,7 @@ class WC_Order_Proxies {
 
 		// Write the checkbox for the product option
 		woocommerce_wp_checkbox( [
-			'id'            => 'order_proxy',
+			'id'            => 'require_order_proxy',
 			'wrapper_class' => 'order-proxies-checkbox',
 			'label'         => __( 'Ask for a proxy?', 'woocommerce-order-proxies' ),
 			'description'   => __( 'If checked, we will show a field during checkout to ask for the name of a proxy.', 'woocommerce-order-proxies' ),
@@ -45,7 +55,65 @@ class WC_Order_Proxies {
 	 */
 	public function write_panel_save( $post_id ) {
 		// Toggle the checkbox
-		update_post_meta( $post_id, 'order_proxy', empty( $_POST['order_proxy'] ) ? 'no' : 'yes' );
+		update_post_meta( $post_id, 'require_order_proxy', empty( $_POST['require_order_proxy'] ) ? 'no' : 'yes' );
+	}
+
+	public function add_checkout_field( $checkout ) {
+		// Set to not ask for a proxy by default
+		$ask_for_proxy = false;
+
+		// Get the global cart and run through items so that we can figure out if we need to add the checkout field
+		foreach ( WC()->cart->get_cart() as $item ) {
+			$product_id = $item['product_id'];
+			$product_order_proxy = get_post_meta( $product_id, 'require_order_proxy', true );
+
+			if ( $product_order_proxy === 'yes' ) {
+				$ask_for_proxy = true;
+				break;
+			}
+		}
+
+		if ( $ask_for_proxy ) {
+			echo '<div class="order-proxies-checkout-field-wrap">';
+
+		    woocommerce_form_field( 'order_proxy', [
+			    'type'          => 'text',
+			    'class'         => ['order-proxy form-row-wide'],
+			    'label'         => __( 'Order Proxy', 'woocommerce-order-proxies' ),
+			    'placeholder'   => __( 'Please enter the name of a proxy for this order', 'woocommerce-order-proxies' ),
+			    'required'      => true,
+		    ], $checkout->get_value( 'order_proxy' ) );
+
+			echo '</div>';
+		}
+
+		return false;
+	}
+
+	public function validate_checkout_field() {
+		if ( ! $_POST['order_proxy'] ) {
+			wc_add_notice( __( 'Items in your cart require the proxy field to be filled in. If you are picking up yourself add "Self" or "N/A".', 'woocommerce-order-proxies') );
+		}
+	}
+
+	public function save_checkout_field( $order_id ) {
+		if ( ! empty( $_POST['order_proxy'] ) ) {
+			update_post_meta( $order_id, 'order_proxy', sanitize_text_field( $_POST['my_field_name'] ) );
+		}
+	}
+
+	public function display_order_proxy_order_admin( $order ) {
+		$order_proxy = get_post_meta( $order->id, 'order_proxy', true );
+
+		if ( $order_proxy ) {
+			?>
+			<p>
+				<strong><?php echo __( 'Order Proxy', 'woocommerce-order-proxies' ); ?>:</strong>
+				<br>
+				<?php echo esc_html( $order_proxy ); ?>
+			</p>
+			<?php
+		}
 	}
 }
 
